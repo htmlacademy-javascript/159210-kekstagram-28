8de3ae19b9cf69367c100af1c60d84461ede720d
slipfly <slipfly@gmail.com>
2023-03-25 15:51:58 +0300

    кнопка "загрузить ещё" запрограммирована

--- js/render-big-pic.js
+++ js/render-big-pic.js
@@ -12,6 +12,10 @@ const descriptionPhoto = bigPicture.querySelector('.social__caption');
 const socialCommentCount = social.querySelector('.social__comment-count');
 const commentsLoader = social.querySelector('.comments-loader');
 
+const START_COMMENTS_COUNT = 5;
+let commentsShown = 0, commentsLeft = 0;
+let commentsArray = [];
+
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -24,18 +28,44 @@ const onMouseClose = (evt) => {
   closeBigPic();
 };
 
-const renderComments = (comments) => {
-  allComments.innerHTML = '';
-  for (let i = 0; i < comments.length; i++) {
+const renderComments = (count) => {
+  socialCommentCount.textContent = `${commentsShown} из ${commentsArray.length}`;
+  for (let i = 0; i < count; i++) {
     const li = document.createElement('li');
     li.classList.add('social__comment');
     li.innerHTML = `<img
                         class="social__picture"
-                        src="${comments[i].avatar}"
-                        alt="${comments[i].name}"
+                        src="${commentsArray[i].avatar}"
+                        alt="${commentsArray[i].name}"
                         width="35" height="35">
-              <p class="social__text">${comments[i].message}</p>`;
+              <p class="social__text">${commentsArray[i].message}</p>`;
     allComments.appendChild(li);
+  }
+};
+
+const renderAllComments = () => {
+  allComments.innerHTML = '';
+
+  if (commentsArray.length > START_COMMENTS_COUNT) {
+    if (commentsLeft > START_COMMENTS_COUNT) {
+      commentsShown += START_COMMENTS_COUNT;
+      commentsLeft -= START_COMMENTS_COUNT;
+
+      renderComments(commentsShown);
+    } else {
+      commentsShown += commentsLeft;
+
+      renderComments(commentsShown);
+      commentsLoader.classList.add('hidden');
+    }
+    if (commentsLeft <= 0) {
+      commentsLoader.classList.add('hidden');
+    }
+  } else {
+    commentsShown = commentsArray.length;
+
+    renderComments(commentsArray.length);
+    commentsLoader.classList.add('hidden');
   }
 };
 
@@ -44,34 +74,41 @@ const renderBigPic = ({url, likes, comments, description}) => {
   likesCount.textContent = likes;
   commentsCount.textContent = comments.length;
   descriptionPhoto.textContent = description;
-  renderComments(comments);
+  commentsLeft = comments.length;
+  renderAllComments();
 };
 
 function openBigPic(url) {
   renderBigPic(url);
-
-  socialCommentCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
 
   bigPicture.classList.remove('hidden');
   document.body.classList.add('modal-open');
 
   document.addEventListener('keydown', onDocumentKeydown);
   bigPicture.querySelector('.cancel').addEventListener('click', onMouseClose);
+  commentsLoader.addEventListener('click', renderAllComments);
 }
 
 function closeBigPic() {
+  commentsShown = 0;
+  commentsLeft = 0;
+
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
 
+  socialCommentCount.classList.remove('hidden');
+  commentsLoader.classList.remove('hidden');
+
   document.removeEventListener('keydown', onDocumentKeydown);
   bigPicture.querySelector('.cancel').removeEventListener('click', onMouseClose);
+  commentsLoader.removeEventListener('click', renderAllComments);
 }
 
 pictures.addEventListener('click', (evt) => {
   if(evt.target.closest('.picture')) {
     const url = String(evt.target.src).slice(String(evt.target.src).indexOf('photos'));
     const thisPost = posts.filter((post) => post.url === url)[0];
+    commentsArray = thisPost.comments;
     openBigPic(thisPost);
   }
 });
